cf3b4e626a3eba866fa2c0381c8ec7d8971aa930
CheoR <cheo.roman+github@gmail.com>
2022-09-18 11:45:34 -0500

    build: attempt automate build with gh actions

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,7 +9,7 @@ module.exports = {
     siteUrl: `https://cheor.github.io/`,
     author: "CheoR",
     description: "CheoR Web Dev Portfolio",
-    image: "./src/images/icon.png", // "/assets/images/self.png",
+    image: "/assets/images/self.png",
     keywords: "CheoR Web Dev Portfolio",
   },
   plugins: [
