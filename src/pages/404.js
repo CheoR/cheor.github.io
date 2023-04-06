801ffa897385dbab2ed2b4866b10b178bcf3a87c
CheoR <cheo.roman+github@gmail.com>
2023-04-06 08:54:43 -0500

    ref: 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,51 +1,31 @@
 import React from "react";
+import { Box, Button, Typography } from "@mui/material";
 import { Link } from "gatsby";
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-};
-
-const paragraphStyles = {
-  marginBottom: 48,
-};
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-};
 
 const NotFoundPage = () => {
   return (
-    <main style={pageStyles}>
-      <title>Not found</title>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry{" "}
-        <span role="img" aria-label="Pensive emoji">
+    <Box component="main">
+      <title>Page Not found</title>
+      <Typography
+        component="div"
+        display="flex"
+        gutterBottom
+        sx={{ justifyContent: "center" }}
+        variant="h5"
+      >
+        Page not found{" "}
+        <Box aria-label="Pensive emoji" component="span" ml={2} role="img">
           😔
-        </span>{" "}
-        we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
-    </main>
+        </Box>
+      </Typography>
+      <Box display="flex" sx={{ justifyContent: "center" }}>
+        <Link to={`/`}>
+          <Button size="small" variant="contained">
+            Go Home
+          </Button>
+        </Link>
+      </Box>
+    </Box>
   );
 };
 
