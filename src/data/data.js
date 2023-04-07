4183a3591f23307fe3f2174aef9b924227b29dec
CheoR <cheo.roman+github@gmail.com>
2023-04-07 09:01:57 -0500

    chore: tech icons

--- src/data/data.js
+++ src/data/data.js
@@ -1,90 +1,3 @@
-export const PROJECTS = [
-  {
-    id: "8a37d3ee-737d-4f4f-9908-6ee2443f31de",
-    preview: "",
-    repo: "https://github.com/CheoR/pack-it-up-v2-server",
-    link: "",
-    aria: "GitHub Project: PackItUpV2",
-    label: "PackItUpV2",
-    description:
-      "PackItUpV2 is a mobile app version of PackItUp, moving/inventory app that helps users track what they pack. ",
-    tags: [
-      "Cypress",
-      "Mobile",
-      "MongoDB",
-      "MUI",
-      "React-Native",
-      "Redux",
-      "TDD",
-    ],
-  },
-  {
-    id: "a00d3362-b7c5-4a6d-8182-b976d454056d",
-    preview: "",
-    repo: "https://github.com/CheoR/shipItOut-client",
-    link: "https://shipitout.herokuapp.com/",
-    aria: "GitHub Project: Ship It Out",
-    label: "ShipItOut",
-    description:
-      "ShipItOut is my full stack NSS C46 capstone project. It is related to my initial project, PackItUp. ShipItOut consist of React front-end and Django backend. It is a dashboard-like app that tracks shipping containers where the shipper, container, port authorities can update the container status as it moves. ",
-    tags: ["Django", "Formik", "Heroku", "Node", "MUI", "React"],
-  },
-  {
-    id: "c6409e2a-8f85-4141-b6fc-1c08315ffb98",
-    preview: "",
-    repo: "https://github.com/CheoR/pack-it-up",
-    link: "https://cr-demo--packitup.netlify.app/",
-    aria: "GitHub Project: Pack It Up",
-    label: "PackItUp",
-    description:
-      "NSS front-end capstone project. PackItUp is a moving app to help users keep track of what items went into what box for moving/storage. ",
-    tags: ["React"],
-  },
-  {
-    id: "61a1783e-a60e-4aba-a412-f8dc00f5adee",
-    preview: "",
-    repo: "https://github.com/CheoR/BeerMe",
-    link: "",
-    aria: "GitHub Project: Beer Me",
-    label: "BeerMe",
-    description:
-      "TDD project with React Frontend and Django backend. Testing done with Jest/Enzyme and Selenium. ",
-    tags: ["Enzyme", "Formik", "React", "Yup"],
-  },
-  {
-    id: "37e42ceb-e3fc-4531-9fa4-dac1f02fa3ef",
-    preview: "",
-    repo: "https://github.com/CheoR/recommend_wine",
-    link: "",
-    aria: "GitHub Project: Wine Recommender",
-    label: "Wine Recommender",
-    description:
-      "Wine recommender k-means clustering to recommend wines based on user preferences and previous wine scores. This project uses TDD approach, Django backend and Travis-CI for deployment. ",
-    tags: ["CI", "Django", "TDD"],
-  },
-  // {
-  //   id: "c3e4c2a5-c5cf-4c70-920f-7cdf7c246118",
-  //   preview: "",
-  //   repo: "https://github.com/CheoR/FreeCodeCampProjects",
-  //   link: "",
-  //   aria: "GitHub Project: Free Code Camp Projects",
-  //   label: "FCC Projects",
-  //   description:
-  //     "FreeCodeCamp projects using HTML, CSS, React, Bootstrap and JavaScript. ",
-  //   tags: ["CSS", "HTML", "JavaScript"],
-  // },
-  // {
-  //   id: "de5fad68-95eb-4b5c-9d15-af576b845280",
-  //   preview: "",
-  //   repo: "https://codepen.io/CheoR/full/QzPJbQ",
-  //   link: "",
-  //   aria: "GitHub Project: Code Pen Projects",
-  //   label: "CodePen",
-  //   description: "Pens",
-  //   tags: ["CSS", "HTML", "JavaScript"],
-  // },
-];
-
 export const CHIPS = [
   // languages
   {
@@ -232,6 +145,79 @@ export const CHIPS = [
   },
 ];
 
+export const ICONS = [
+  {
+    alt: "cypress",
+    href: "https://www.cypress.io",
+    src: "https://raw.githubusercontent.com/simple-icons/simple-icons/6e46ec1fc23b60c8fd0d2f2ff46db82e16dbd75f/icons/cypress.svg",
+  },
+  {
+    alt: "django",
+    href: "https://www.djangoproject.com/",
+    src: "https://cdn.worldvectorlogo.com/logos/django.svg",
+  },
+  {
+    alt: "figma",
+    href: "https://www.figma.com/",
+    src: "https://www.vectorlogo.zone/logos/figma/figma-icon.svg",
+  },
+  {
+    alt: "gatsby",
+    href: "https://www.gatsbyjs.com/",
+    src: "https://www.vectorlogo.zone/logos/gatsbyjs/gatsbyjs-icon.svg",
+  },
+  {
+    alt: "jest",
+    href: "https://jestjs.io",
+    src: "https://www.vectorlogo.zone/logos/jestjsio/jestjsio-icon.svg",
+  },
+  {
+    alt: "linux",
+    href: "https://www.linux.org/",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/linux/linux-original.svg",
+  },
+  {
+    alt: "mongodb",
+    href: "https://www.mongodb.com/",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/mongodb/mongodb-original-wordmark.svg",
+  },
+  {
+    alt: "nextjs",
+    href: "https://nextjs.org/",
+    src: "https://cdn.worldvectorlogo.com/logos/nextjs-2.svg",
+  },
+  {
+    alt: "postgresql",
+    href: "https://www.postgresql.org",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/postgresql/postgresql-original-wordmark.svg",
+  },
+  {
+    alt: "postman",
+    href: "https://postman.com",
+    src: "https://www.vectorlogo.zone/logos/getpostman/getpostman-icon.svg",
+  },
+  {
+    alt: "python",
+    href: "https://www.python.org",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+  },
+  {
+    alt: "redis",
+    href: "https://redis.io",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/redis/redis-original-wordmark.svg",
+  },
+  {
+    alt: "sqlite",
+    href: "https://www.sqlite.org/",
+    src: "https://www.vectorlogo.zone/logos/sqlite/sqlite-icon.svg",
+  },
+  {
+    alt: "typescript",
+    href: "https://www.typescriptlang.org/",
+    src: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+  },
+];
+
 export const PAGES = [
   {
     url: "/",
@@ -252,6 +238,93 @@ export const PAGES = [
   // {
   //   url: "/v1", // https://cheor.github.io/v1/",
   //   page: "Portfolio V1",
+  // },
+];
+
+export const PROJECTS = [
+  {
+    id: "8a37d3ee-737d-4f4f-9908-6ee2443f31de",
+    preview: "",
+    repo: "https://github.com/CheoR/pack-it-up-v2-server",
+    link: "",
+    aria: "GitHub Project: PackItUpV2",
+    label: "PackItUpV2",
+    description:
+      "PackItUpV2 is a mobile app version of PackItUp, moving/inventory app that helps users track what they pack. ",
+    tags: [
+      "Cypress",
+      "Mobile",
+      "MongoDB",
+      "MUI",
+      "React-Native",
+      "Redux",
+      "TDD",
+    ],
+  },
+  {
+    id: "a00d3362-b7c5-4a6d-8182-b976d454056d",
+    preview: "",
+    repo: "https://github.com/CheoR/shipItOut-client",
+    link: "https://shipitout.herokuapp.com/",
+    aria: "GitHub Project: Ship It Out",
+    label: "ShipItOut",
+    description:
+      "ShipItOut is my full stack NSS C46 capstone project. It is related to my initial project, PackItUp. ShipItOut consist of React front-end and Django backend. It is a dashboard-like app that tracks shipping containers where the shipper, container, port authorities can update the container status as it moves. ",
+    tags: ["Django", "Formik", "Heroku", "Node", "MUI", "React"],
+  },
+  {
+    id: "c6409e2a-8f85-4141-b6fc-1c08315ffb98",
+    preview: "",
+    repo: "https://github.com/CheoR/pack-it-up",
+    link: "https://cr-demo--packitup.netlify.app/",
+    aria: "GitHub Project: Pack It Up",
+    label: "PackItUp",
+    description:
+      "NSS front-end capstone project. PackItUp is a moving app to help users keep track of what items went into what box for moving/storage. ",
+    tags: ["React"],
+  },
+  {
+    id: "61a1783e-a60e-4aba-a412-f8dc00f5adee",
+    preview: "",
+    repo: "https://github.com/CheoR/BeerMe",
+    link: "",
+    aria: "GitHub Project: Beer Me",
+    label: "BeerMe",
+    description:
+      "TDD project with React Frontend and Django backend. Testing done with Jest/Enzyme and Selenium. ",
+    tags: ["Enzyme", "Formik", "React", "Yup"],
+  },
+  {
+    id: "37e42ceb-e3fc-4531-9fa4-dac1f02fa3ef",
+    preview: "",
+    repo: "https://github.com/CheoR/recommend_wine",
+    link: "",
+    aria: "GitHub Project: Wine Recommender",
+    label: "Wine Recommender",
+    description:
+      "Wine recommender k-means clustering to recommend wines based on user preferences and previous wine scores. This project uses TDD approach, Django backend and Travis-CI for deployment. ",
+    tags: ["CI", "Django", "TDD"],
+  },
+  // {
+  //   id: "c3e4c2a5-c5cf-4c70-920f-7cdf7c246118",
+  //   preview: "",
+  //   repo: "https://github.com/CheoR/FreeCodeCampProjects",
+  //   link: "",
+  //   aria: "GitHub Project: Free Code Camp Projects",
+  //   label: "FCC Projects",
+  //   description:
+  //     "FreeCodeCamp projects using HTML, CSS, React, Bootstrap and JavaScript. ",
+  //   tags: ["CSS", "HTML", "JavaScript"],
+  // },
+  // {
+  //   id: "de5fad68-95eb-4b5c-9d15-af576b845280",
+  //   preview: "",
+  //   repo: "https://codepen.io/CheoR/full/QzPJbQ",
+  //   link: "",
+  //   aria: "GitHub Project: Code Pen Projects",
+  //   label: "CodePen",
+  //   description: "Pens",
+  //   tags: ["CSS", "HTML", "JavaScript"],
   // },
 ];
 
